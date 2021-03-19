d0de18fb81b6252415b192ceb21e5bf40b6bde9f
Bruno <brunobucard@gmail.com>
2021-03-19 16:31:32 -0300

    refactoring the list of categories

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -2,15 +2,13 @@ import { Response, Request } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-
-constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
-  
     this.createCategoryUseCase.execute({ name, description });
-  
+
     return response.status(201).send();
   }
 }
