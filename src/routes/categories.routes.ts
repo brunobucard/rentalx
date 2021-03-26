d9eddbb73b5b9eb06536109218389cef639fc65f
Bruno Bucard <brunobucard@gmail.com>
2021-03-25 23:46:58 -0300

    refactoring use case categorie

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import createCategoryController from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 import {importCategoryController} from "../modules/cars/useCases/importCategory";
 
@@ -12,7 +12,8 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);  
+  console.log("Rota");
+  return createCategoryController().handle(request, response);  
 });
 
 categoriesRoutes.get("/", (request, response) => {
