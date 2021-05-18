b8ab31e562d649072d11812dc87ad06728b4f6a0
Bruno Bucard <brunobucard@gmail.com>
2021-05-17 22:06:19 -0300

    Use case car image registration

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecifica
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 
 
 // IcategoriesRepository
@@ -28,3 +30,7 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+
+
+container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository);
