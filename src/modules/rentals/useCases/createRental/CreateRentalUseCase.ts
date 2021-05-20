4ac12d6af43b21fd87d5e7a90760c587a5f0778c
Bruno Bucard <brunobucard@gmail.com>
2021-05-20 06:33:13 -0300

    rental registration

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -18,7 +19,7 @@ class CreateRentalUseCase {
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> { //alterar para <Rental>
+  }: IRequest): Promise<Rental> { 
     // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
@@ -35,6 +36,13 @@ class CreateRentalUseCase {
 
    // O aluguel deve ter duração mínima de 24 horas.
 
+   const rental = await this.rentalsRepository.create({
+     user_id,
+     car_id,
+     expected_return_date,
+   });
+
+   return rental;
     
   }
 }
